6cf11327cf455532a8a7816321a1023bff13e78c
Dmitrii Ostrovskii <ostrodmit@gmail.com>
2020-07-06 18:25:02 -0700

    Some comments added

--- test_FGM.py
+++ test_FGM.py
@@ -16,15 +16,20 @@ def quad_func(A,b,x):
     f = ( (la.norm(np.matmul(A,x) - b)) ** 2 )/2
     return(f)
 
+# Testing FGM on the unconstrained quadratic problem min_x (Ax-b)^2/2 
 d = 30
 T = 10**3;
 A = np.diag(range(1,d+1))
 b = np.ones([d,1])
+# Stepsize calculation 
 B = np.multiply(A.transpose(), A)
 [l,v] = la.eigh(B)
 L = np.max(l)
 gam = 1/L
+
+# Defining gradient oracle
 grad = lambda x: quad_grad(A,b,x)
+
 z0 = np.zeros(d)
 opt_sol = la.solve(A,b)
 print(opt_sol)
