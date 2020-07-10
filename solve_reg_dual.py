cd4b3dc2bd511c7c14f7e5f6b7600796a9818f71
Dmitrii Ostrovskii <ostrodmit@gmail.com>
2020-07-09 21:54:20 -0700

    Debugging

--- solve_reg_dual.py
+++ solve_reg_dual.py
@@ -5,8 +5,8 @@
 from restart_fgm import restart_fgm
 
 def solve_reg_dual(y,x_prv,y_bar,gam_x,lam_y,T,S,Rx,Gx,Gy):
-    g_reg_x = lambda x: Gx(x,y) - 1/gam_x * (x-x_prv)
+    g_reg_x = lambda x: Gx(x,y) + 1/gam_x * (x-x_prv)
     xx = restart_fgm(x_prv,Rx,2/3*gam_x,T,S,g_reg_x)[0]
     x_nxt = xx[:,S]
-    g_reg_y = -Gy(x_nxt,y) + lam_y * (y-y_bar)
+    g_reg_y = -Gy(x_nxt,y) #+ lam_y * (y-y_bar)
     return x_nxt, g_reg_y
--- test_fast_nash.py
+++ test_fast_nash.py
@@ -9,13 +9,13 @@ from fast_nash import fast_nash
 from quadratic import quad_grad, quad_func
 
 # Testing fast_nash and competitors on the nonconvex-strongly-concave problem
-# min_x \max_y \|A*x - y\|_2^2/2 - \lam_y \|y - b\|^2/2
+# min_x \max_y |A*x - y|_2^2/2 - lam_y |y - b|^2/2
 # with b the all-ones vector and A the discrete derivative matrix
 
-d = 10
-kap_y = 1e2
-delta = 1e-4
-Tx = int(100)
+d = 2
+kap_y = 1e1
+delta = 1e-2
+Tx = int(10)
 
 # discrete difference matrix [[1,0,...,0], [-1,1,0,...,0],..., [0,...,0,-1,1]]
 I = np.identity(d)
@@ -32,14 +32,15 @@ Lyy = 1
 Lxy = np.sqrt(Lyy)
 Lyy_plus = Lyy + Lxy**2/Lxx
 lam_y = Lyy_plus/kap_y
+#lam_y = 0
 Ry=2*la.norm(b)
-Gap = la.norm(b)**2/2
+Gap = lam_y * la.norm(b)**2/2
 
 # Defining oracle
 Gx = lambda x,y: quad_grad(A,y,x)
 def Gy(x,y):
     z = np.matmul(A,x)
-    return quad_grad(I,z,y) - quad_grad(I,b,y)
+    return quad_grad(I,z,y) - lam_y * quad_grad(I,b,y)
 func = lambda x,y: quad_func(A,y,x) - lam_y * quad_func(I,b,y)
 
 # Initializing input parameters for fast_nash
@@ -50,7 +51,7 @@ Ty = int(np.sqrt(4*(kap_y+1)))
 Theta_plus = Lyy_plus*(Ry**2)
 Sy = int(np.ceil(2*np.log2(max([Ty,Theta_plus/delta]))))
 To = 11
-OverGap = 72*(3*Gap+2*Theta_plus+6*lam_y*Ry**2)
+OverGap = 72*(3*Gap+2*Theta_plus+6*lam_y*(Ry**2))
 So \
 = int(np.ceil(np.log2(OverGap * (Tx/Gap + 2*Theta_plus/(delta**2) + 1/(12*delta)))/2))
 
@@ -60,11 +61,12 @@ x_opt = la.lstsq(A,b,rcond=None)[0]
 Rx = 2*la.norm(x_opt)
 
 # Running fast_nash
-x, y, Gx_norm, x_best, y_best, Gx_norm_best  \
+x, y, Gx_norm, Gy_norm, x_best, y_best, Gx_norm_best  \
 = fast_nash(Gx,Gy,d,d,Rx,Ry,x0,y_bar,Tx,Ty,Sy,gam_x,gam_y,lam_y,To,So)
 
 # Plotting
 plt.plot(Gx_norm,color='red')
+plt.plot(Gy_norm,color='green')
 plt.xscale('log'); plt.yscale('log')
 plt.show()
 
